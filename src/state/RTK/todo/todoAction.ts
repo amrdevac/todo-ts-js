bdbb83deea6a4a84065c86b605b4e969fb3dd893
amrdevac <amrullahdev@yahoo.com>
2023-11-21 23:11:35 +0700

    enhance todo action ,

--- src/state/RTK/todo/todoAction.ts
+++ src/state/RTK/todo/todoAction.ts
@@ -2,12 +2,30 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import axios from "axios";
 
-export const todoAction = createAsyncThunk(
+ type paramTypeKey = 'list' | 'add' | 'delete' | 'update';
+interface todoActionType {
+  type: paramTypeKey,
+  param?: any
+}
+
+interface todoActionRes {
+  action_type: string,
+  action_response: any
+}
+
+export const todoAction = createAsyncThunk<todoActionRes, todoActionType, { state: RootState }>(
   "todoAction",
-  async (_, thunkAPI) => {
-    const { todo: mainState } = thunkAPI.getState() as RootState;
-    mainState.request.filter;
-    const response = await axios.post("/api/todo/list");
-    return { action_type: "list", action_response: response };
+  async (param, thunkAPI) => {
+    try {
+      let response;
+      const { todo: mainState } = thunkAPI.getState() as RootState;
+      mainState.request.filter;
+      if (param.type == "list") {
+        response = await axios.post("/api/todo/list");
+      }
+      return { action_type: param.type, action_response: response };
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
   }
 );
