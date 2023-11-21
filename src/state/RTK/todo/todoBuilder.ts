f8d6e9fc8c0fa0ddd423e47c38ff2e54cc873ac7
amrdevac <amrullahdev@yahoo.com>
2023-11-21 23:11:41 +0700

    enhance todo builder

--- src/state/RTK/todo/todoBuilder.ts
+++ src/state/RTK/todo/todoBuilder.ts
@@ -1,6 +1,24 @@
-import { ActionReducerMapBuilder } from "@reduxjs/toolkit";
-import { RootState } from "../store";
+import { ActionReducerMapBuilder, AsyncThunk } from "@reduxjs/toolkit";
+import { initType, xhrResponseArrData } from "./todoSlice";
+import { todoAction } from "./todoAction";
 
-export const todoBuilder = (builder, asyncService) => {
-  builder.addCase();
+export const todoBuilder = (builder: ActionReducerMapBuilder<initType>,
+  asyncService: typeof todoAction) => {
+  builder.addCase(asyncService.pending, (state) => {
+    state.xhr.isLoading = true
+  });
+  builder.addCase(asyncService.fulfilled, (state, action) => {
+    state.xhr.isLoading = false;
+    state.xhr.isError = false;
+
+    if (action.payload.action_type == "list") {
+      state.arr_data = (action.payload.action_response as xhrResponseArrData).data.response_data.data;
+    }
+
+  })
+  builder.addCase(asyncService.rejected, (state, action) => {
+    state.xhr.isLoading = false;
+    state.xhr.isError = true;
+    state.xhr.responseMessage = action.error.toString()
+  })
 };
