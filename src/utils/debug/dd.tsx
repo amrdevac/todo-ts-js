c60a20bc5d7ae41e5fe37f1b4fe7dcddd188f1bd
Fahmi Amrullah <3276111905960001>
2023-11-22 16:07:38 +0700

    enhance diedump

--- src/utils/debug/dd.tsx
+++ src/utils/debug/dd.tsx
@@ -1,8 +1,8 @@
 export const dd = (data: any) => {
   return (
     <pre
-      className=""
-      style={{ background: "black", color: "lightgreen", padding: 10 }}
+      className="w-full overflow-scroll"
+      style={{ background: "black", color: "lightgreen", padding: 10 , width:"100%", overflow:"auto" }}
     >
       {JSON.stringify(data, null, "\t")};
     </pre>
