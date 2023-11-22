7f99a8c5f5d5e10ad92aadb6f7102bad21a1f104
Fahmi Amrullah <3276111905960001>
2023-11-22 16:07:29 +0700

    enhance action

--- src/state/RTK/todo/todoAction.ts
+++ src/state/RTK/todo/todoAction.ts
@@ -2,30 +2,35 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import axios from "axios";
 
- type paramTypeKey = 'list' | 'add' | 'delete' | 'update';
+type paramTypeKey = "list" | "add" | "delete" | "update";
 interface todoActionType {
-  type: paramTypeKey,
-  param?: any
+  type: paramTypeKey;
+  param?: any;
 }
 
 interface todoActionRes {
-  action_type: string,
-  action_response: any
+  action_type: string;
+  action_response: any;
 }
 
-export const todoAction = createAsyncThunk<todoActionRes, todoActionType, { state: RootState }>(
-  "todoAction",
-  async (param, thunkAPI) => {
-    try {
-      let response;
-      const { todo: mainState } = thunkAPI.getState() as RootState;
-      mainState.request.filter;
-      if (param.type == "list") {
-        response = await axios.post("/api/todo/list");
-      }
-      return { action_type: param.type, action_response: response };
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
+export const todoAction = createAsyncThunk<
+  todoActionRes,
+  todoActionType,
+  { state: RootState }
+>("todoAction", async (param, thunkAPI) => {
+  try {
+    let response;
+    const { todo: mainState } = thunkAPI.getState() as RootState;
+    mainState.request.filter;
+    if (param.type == "list") {
+      response = await axios.post("/api/todo/list");
     }
+
+    if (param.type == "add") {
+      response = await axios.post("/api/todo/add", mainState.form);
+    }
+    return { action_type: param.type, action_response: response };
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
   }
-);
+});
